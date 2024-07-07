849a20fe3be7d857aee3e9fb159e9128b79ccb30
Kayo Souza <kayo.felipe.souza2014@gmail.com>
2024-07-06 23:34:00 -0300

    Implement Firebase registration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
 	id("com.android.application")
+	id("com.google.gms.google-services")
 }
 
 android {
@@ -8,7 +9,7 @@ android {
 
 	defaultConfig {
 		applicationId = "com.example.telalogin"
-		minSdk = 30
+		minSdk = 28
 		targetSdk = 34
 		versionCode = 1
 		versionName = "1.0"
@@ -32,6 +33,10 @@ dependencies {
 	implementation("androidx.appcompat:appcompat:1.7.0")
 	implementation("com.google.android.material:material:1.12.0")
 	implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+	implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
+	implementation("com.google.firebase:firebase-auth")
+	implementation("com.google.firebase:firebase-firestore")
+	implementation("de.hdodenhof:circleimageview:3.1.0")
 	testImplementation("junit:junit:4.13.2")
 	androidTestImplementation("androidx.test.ext:junit:1.2.1")
 	androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
